a9c8552ebbcc3c94a90c3e8c65c96fbf3a0a70f8
Piotr <piotr.ostrowski@aol.com>
2022-04-06 11:43:28 +0200

    asdf

--- generate.py
+++ generate.py
@@ -16,3 +16,10 @@ def generate_readme(title: str, description: str, repo_name: str):
     )
     with open("./GENERATED.md", "w+") as f:
         f.write(readme)
+
+
+if __name__ == "__main__":
+    title = "README.md generator"
+    description = "this just generates README.md in this fashion"
+    repo_name = "readme_generator"
+    generate_readme(title, description, repo_name)
--- main.py
+++
@@ -1,7 +0,0 @@
-from generate import generate_readme
-
-if __name__ == "__main__":
-    title = "README.md generator"
-    description = "this just generates README.md in this fashion"
-    repo_name = "readme_generator"
-    generate_readme(title, description, repo_name)
